eeb42ca153f19046f8416cb1bd28c3cd8ad58e8c
zainjhere <zain.javed@softvira.com>
2024-03-05 11:50:37 +0500

    Sort Issues

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,54 +1,82 @@
-import React from 'react';
-import {Table} from "@radix-ui/themes"
-import prisma from '@/prisma/client';
-import IssueStatusBadge from '../components/IssueStatusBadge';
-import delay from 'delay';
-import IssueActions from './IssueActions';
-import Link from '../components/Link';
-import { Status } from '@prisma/client';
+import React from "react";
+import { Table } from "@radix-ui/themes";
+import prisma from "@/prisma/client";
+import IssueStatusBadge from "../components/IssueStatusBadge";
+import NextLink from "next/link";
+import IssueActions from "./IssueActions";
+import Link from "../components/Link";
+import { Issue, Status } from "@prisma/client";
+import { ArrowUpIcon } from "@radix-ui/react-icons";
 
-const IssuesPage = async ({searchParams} : {searchParams: {status: Status}}) => {
+interface Props {
+  searchParams: {status: Status, orderBy: keyof Issue}
+}
+
+const IssuesPage = async ({
+  searchParams
+}: Props ) => {
+  const columns: { label: string; value: keyof Issue; className?: string }[] = [
+    { label: "Issue", value: "title" },
+    { label: "Status", value: "status", className: "hidden md:table-cell" },
+    { label: "Created", value: "createdAt", className: "hidden md:table-cell" },
+  ];
+
   const statuses = Object.values(Status);
-  const status = statuses.includes(searchParams.status) ? searchParams.status : undefined;
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
+
+  const orderBy = columns.map(column => column.value).includes(searchParams.orderBy) ? {[searchParams.orderBy]: 'asc'} : undefined;
 
   const issues = await prisma.issue.findMany({
     where: {
-      status
-    }
+      status,
+    },
+    orderBy
   });
 
   return (
     <div>
       <IssueActions />
-      <Table.Root variant='surface'>
+      <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
-            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className='hidden md:table-cell'>Status</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className='hidden md:table-cell'>Created</Table.ColumnHeaderCell>
+            {columns.map((column) => (
+              <Table.ColumnHeaderCell key={column.value}>
+                {" "}
+                <NextLink href={{
+                  query: {...searchParams, orderBy: column.value}
+                }}> {column.label} </NextLink>{" "}
+                {column.value === searchParams.orderBy && <ArrowUpIcon className="inline" /> }
+              </Table.ColumnHeaderCell>
+            ))}
           </Table.Row>
         </Table.Header>
         <Table.Body>
           {issues.map((issue) => (
             <Table.Row key={issue.id}>
               <Table.Cell>
-                <Link href={`/issues/${issue.id}`}>
-                  {issue.title}
-                </Link>   
-                <div className='block md:hidden'>
+                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+                <div className="block md:hidden">
                   <IssueStatusBadge status={issue.status} />
                 </div>
               </Table.Cell>
-              <Table.Cell className='hidden md:table-cell'> <IssueStatusBadge status={issue.status} /> </Table.Cell>
-              <Table.Cell className='hidden md:table-cell'> {issue.createdAt.toDateString()} </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {" "}
+                <IssueStatusBadge status={issue.status} />{" "}
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {" "}
+                {issue.createdAt.toDateString()}{" "}
+              </Table.Cell>
             </Table.Row>
           ))}
         </Table.Body>
       </Table.Root>
     </div>
-  )
-}
+  );
+};
 
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 
-export default IssuesPage
+export default IssuesPage;

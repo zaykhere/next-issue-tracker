e6e7915721e7c5ceecbf4b6cb97523daa91d48a0
zain <zainjhere@gmail.com>
2024-03-09 19:40:20 +0500

    Use react cache for fetching issue

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -2,7 +2,7 @@
 import prisma from "@/prisma/client";
 import { Box, Flex, Grid } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
-import React from "react";
+import React, { cache } from "react";
 import EditIssueButton from "./EditIssueButton";
 import IssueDetails from "./IssueDetails";
 import DeleteIssueButton from "./DeleteIssueButton";
@@ -13,12 +13,13 @@ interface Props {
   params: { id: string };
 }
 
+const fetchUser = cache((issueId: number) => prisma.issue.findUnique({where: {id: issueId}}));
+
+
 const IssueDetailsPage = async ({ params }: Props) => {
   const session = await getServerSession(authOptions);
 
-  const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id) },
-  });
+  const issue = await fetchUser(parseInt(params.id));
 
   if (!issue) notFound();
 
@@ -42,7 +43,7 @@ const IssueDetailsPage = async ({ params }: Props) => {
 };
 
 export async function generateMetadata({params}: Props) {
-  const issue = await prisma.issue.findUnique({where: {id: parseInt(params.id)}});
+  const issue = await fetchUser(parseInt(params.id));
 
   return {
     title: issue?.title,
